5bce0acbeb73ec720e341671ccced8093915d064
Augusto Castilho <castilho@ice.ufjf.br>
2022-12-27 14:33:09 -0300

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,19 +3,6 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import Icon from 'react-native-vector-icons/AntDesign';
 
-
-// export function Home({ userType }: HomeProps) {
-//     const navigation = useNavigation<any>();
-//     console.log(userType)
-    
-//     return (
-//         <View style={styles.container}>
-//             <TouchableOpacity onPress={() => { }} style={styles.button}>
-//                 <Text style={styles.buttonText}>{userType === "driver" ? "VER SOLICITAÇÕES" : "NOVA CORRIDA"}</Text>
-//             </TouchableOpacity>
-//         </View>
-//     )
-// }
 export function Home({ route }: { route: any}, navigation: any) {
 
     const userType = route.params.userType;
