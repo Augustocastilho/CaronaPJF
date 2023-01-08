e9083d3e8c2f1e203536bddc8f2ec653b1477135
Augusto Castilho <castilho@ice.ufjf.br>
2023-01-08 15:44:24 -0300

    Correção de configurações

--- babel.config.js
+++ babel.config.js
@@ -3,22 +3,49 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // [
+      //   'module-resolver',
+      //   {
+      //     extensions: [
+      //       '.js',
+      //       '.jsx',
+      //       '.ts',
+      //       '.tsx',
+      //       '.android.js',
+      //       '.android.tsx',
+      //       '.ios.js',
+      //       '.ios.tsx'
+      //     ],
+      //     //   root: ['.'],
+      //     //   resolvePath(sourcePath, currentFile, ...otherparams /* opts */) {
+      //     //     return resolvePath(sourcePath, currentFile, otherparams);
+      //     //   },
+      //     //   loglevel: 'verbose',
+
+      //   }
+
+      // ],
       [
-        'module-resolver',
+        require('@babel/plugin-transform-runtime').default,
         {
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx'
-          ],
-          root: ['.']
-        }
-      ]
+          helpers: false,
+          regenerator: true,
+          corejs: false,
+        },
+      ],
+      [
+        require('@babel/plugin-transform-regenerator').default,
+        {
+          async: false,
+        },
+      ],
+      [
+        require('babel-plugin-module-resolver').default,
+        {
+          root: ['./app/javascript/'],
+        },
+      ],
+      // ["@babel/plugin-syntax-class-properties"], 
     ]
   };
 };
